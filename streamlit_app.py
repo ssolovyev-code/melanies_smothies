f89c893ecd82868513f87aee8b0b37cc2ac10e76
ssolovyev-code <sergey_solovyev@freenet.am>
2026-06-11 14:56:39 +0400

    connection to snowflake changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 import os
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -19,7 +19,9 @@ st.write("The name on your cup will be: ---", title)
 
 #st.write("You selected:", option)
 
-session = get_active_session()
+# session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 ingridients_list = st.multiselect(
