fc0b991030d443f21914eb8b96a6ededd8c0f9c1
ssolovyev-code <sergey_solovyev@freenet.am>
2026-06-11 18:45:37 +0400

    Update streamlit_app.py
    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ st.write("The name on your cup will be: ---", title)
 # session = get_active_session()
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.stop()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 ingridients_list = st.multiselect(
