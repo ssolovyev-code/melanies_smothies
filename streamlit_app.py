da39a62bbea49d8d7422aa531bf4828a459919c2
ssolovyev-code <sergey_solovyev@freenet.am>
2026-06-11 18:26:36 +0400

    Update streamlit_app.py
    added nutritious

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,8 @@ ingridients_list = st.multiselect(
 )
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json()) 
+st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = true)
 
 if ingridients_list :
    # st.write(ingridients_list)
